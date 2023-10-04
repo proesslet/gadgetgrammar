e22e6d9a3a8c4cea28a1d0d9caf0717644490790
Preston Roesslet <proesslet@gmail.com>
2023-10-04 15:18:24 -0500

    added router

--- Gadget-Grammar/src/main.js
+++ Gadget-Grammar/src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
 
 // Import bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-createApp(App).mount("#app");
+// Setup router
+app.use(router);
+
+app.mount("#app");
---
+++ Gadget-Grammar/src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Welcome to Gadget Grammar!</h1>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
