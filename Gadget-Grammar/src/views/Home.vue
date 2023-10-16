6e92d3c3bb391f2eefe05ef64ddc22b2cd5bae19
kennyb66 <kenneth.e.bartel-1@ou.edu>
2023-10-16 16:05:14 -0500

    Added 3 buttons for GUI

--- Gadget-Grammar/src/views/Home.vue
+++ Gadget-Grammar/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template> 
     <h1>{{ msg }}</h1>
+    <button type="button" class="button button1">Play</button>
+    <button type="button" class="button button2">Log in</button>
+    <button type="button" class="button button3">How to play</button>
 </template>
 
 <script>
@@ -15,3 +18,24 @@ export default {
   methods: {},
 };
 </script>
+
+<style>
+    .button {
+      border: solid;
+      color: black;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      cursor: pointer;
+    }
+
+    .button1 {
+      top: 60%;
+    };
+    
+    
+
+</style>
+
