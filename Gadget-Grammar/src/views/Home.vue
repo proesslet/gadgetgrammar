fe7da6801df7b4d28bb33cb27efe5f152d4bcb2b
kennyb66 <kenneth.e.bartel-1@ou.edu>
2023-10-18 16:10:42 -0500

    Modal popup for the 'How to Play' button

--- Gadget-Grammar/src/views/Home.vue
+++ Gadget-Grammar/src/views/Home.vue
@@ -12,18 +12,24 @@
         <router-link to="/game" class="btn btn-primary m-2">Play</router-link>
         <button type="button" class="btn btn-primary m-2">Log in</button>
         <button type="button" class="btn btn-primary m-2">How to play</button>
+        <button data-toggle="modal" data-target="#myModal" class="How to play"></button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Modal from "./gameinstructions.vue";
 export default {
-  name: "Home",
-
-  data() {
-    return {};
+  name: "Modal",
+  components: {
+    Modal
   },
-  methods: {},
+  data() {
+    return {
+      right: "right",
+    };
+  },
 };
 </script>
+
