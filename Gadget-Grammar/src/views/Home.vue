963ea09d2065c1b7895e94b0fdf09cff0680564c
Cole <colehicks50@gmail.com>
2023-10-17 21:24:51 -0500

    Added a legend to the game page

--- Gadget-Grammar/src/views/Home.vue
+++ Gadget-Grammar/src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
-  <h1 class="centered-text">{{ msg }}</h1>
-  <Grid />
+  <div>
+    <h1 class="centered-text">{{ msg }}</h1>
+    <Grid />
+    <div class="legend">
+      <div class="legend-item">
+        <div class="color-box green"></div>
+        <span>Correct Letter, Correct Spot</span>
+      </div>
+      <div class="legend-item">
+        <div class="color-box yellow"></div>
+        <span>Correct Letter, Incorrect Spot</span>
+      </div>
+      <div class="legend-item">
+        <div class="color-box gray"></div>
+        <span>Incorrect Letter, Incorrect Spot</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -23,5 +39,36 @@ export default {
 <style>
 .centered-text {
   text-align: center;
+}
+
+.legend {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.legend-item {
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+}
+
+.color-box {
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+  border-radius: 50%;
+}
+
+.green {
+  background-color: green;
+}
+
+.yellow {
+  background-color: yellow;
+}
+
+.gray {
+  background-color: gray;
 }
 </style>
