a5106e8eee8189cb3a8be0b89ddffdffbaa11f1c
Preston Roesslet <proesslet@gmail.com>
2023-10-17 21:40:55 -0500

    style landing page

--- Gadget-Grammar/src/views/Home.vue
+++ Gadget-Grammar/src/views/Home.vue
@@ -1,41 +1,46 @@
-<template> 
-  <h1>{{ msg }}</h1>
-  <button type="button" class="button button1">Play</button>
-  <button type="button" class="button button2">Log in</button>
-  <button type="button" class="button button3">How to play</button>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="text-center">Welcome to Gadget Grammar!</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div
+        class="col-3 d-flex flex-column m-auto h-100 justify-content-space-between"
+      >
+        <button type="button" class="btn btn-primary m-2">Play</button>
+        <button type="button" class="btn btn-primary m-2">Log in</button>
+        <button type="button" class="btn btn-primary m-2">How to play</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "Home",
-  
+
   data() {
-    return {
-      msg: "Welcome to Gadget Grammar!",
-  };
-},
-methods: {},
+    return {};
+  },
+  methods: {},
 };
 </script>
 
 <style>
-  .button {
-    border: solid;
-    color: black;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    cursor: pointer;
-  }
+.button {
+  border: solid;
+  color: black;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+}
 
-  .button1 {
-    top: 60%;
-  };
-  
-  
-
+.button1 {
+  top: 60%;
+}
 </style>
-
