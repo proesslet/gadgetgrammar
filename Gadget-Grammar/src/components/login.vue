a6ff445297156885fe564fd85b4e5401b5717748
Cole Hicks <cth2x@Coles-MacBook-Pro.local>
2023-10-26 02:57:04 -0500

    Login modal is visible, but not functional yet

--- Gadget-Grammar/src/components/login.vue
+++ Gadget-Grammar/src/components/login.vue
@@ -1,57 +1,25 @@
 <template>
-    <div
-      class="modal fade"
-      id="loginmodal"
-      tabindex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-    <div class="modal-dialog modal-lg">
-      <div class="modal-dialog text-dark">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">How to Play</h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <ul>
-              <li>
-                Each guess must be a valid 5-letter technological word.
-              </li>
-              <li>
-                The color of the tiles will change to show the correctness of your guess.
-                <ul>
-                  <li>
-                    If the color of a tile turns green after input, the letter in that tile is in the correct spot.
-                  </li>
-                  <li>
-                    If the color of a tile turns yellow after input, the letter in that tile is in the word, but in the wrong spot
-                  </li>
-                  <li>
-                    If the color of a tile turns gray after input, the letter in that tile is not in the word
-                  </li>
-                </ul>
-              </li>
-            </ul>
-           </div>
+  <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="loginModalLabel">Login</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <!-- Login form will go here -->
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Login</button>
         </div>
       </div>
     </div>
-    </div>
-  </template>
-  <script>
-  export default {
-    name: "Modal",
-    methods: {
-      close() {
-        this.$emit("close");
-      },
-    },
-  };
-  </script>
-  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginModal",
+};
+</script>
--- Gadget-Grammar/src/views/Home.vue
+++ Gadget-Grammar/src/views/Home.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="container">
     <div class="row">
       <div class="col">
@@ -21,6 +21,34 @@
         </button>
       </div>
     </div>
+    <Modal />
+  </div>
+</template> -->
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="text-center">Welcome to Gadget Grammar!</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div
+        class="col-3 d-flex flex-column m-auto h-100 justify-content-space-between"
+      >
+        <router-link to="/game" class="btn btn-primary m-2">Play</router-link>
+        <button type="button" class="btn btn-primary m-2" data-bs-toggle="modal" data-bs-target="#loginModal">Log in</button>
+        <button
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#instructionsModal"
+          class="btn btn-primary m-2"
+        >
+          How to play
+        </button>
+      </div>
+    </div>
+    <LoginModal />
     <Modal />
   </div>
 </template>
