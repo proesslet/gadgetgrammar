d79782ab13b422e57a10ebe3afa950d78d10393a
cth2x <colehicks50@gmail.com>
2023-10-30 15:43:18 -0500

    Create account page added

--- Gadget-Grammar/src/router.js
+++ Gadget-Grammar/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue";
 import Game from "./views/Game.vue";
+import CreateAccount from "./views/CreateAccount.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,6 +13,10 @@ const router = createRouter({
     {
       path: "/game",
       component: Game,
+    },
+    {
+      path: "/create-account",
+      component: CreateAccount,
     },
   ],
 });
