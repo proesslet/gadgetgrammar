3ff4b13615d1d3c484398050be6bd720af11a8d9
Preston Roesslet <proesslet@gmail.com>
2023-11-27 01:57:23 -0600

    change reset streak implementation

--- Gadget-Grammar/src/main.js
+++ Gadget-Grammar/src/main.js
@@ -15,7 +15,7 @@ import "bootstrap";
 // For development, change default to: "http://localhost:3000"
 // For production, change default to: "https://gadgetgrammarservice.onrender.com/"
 // DO NOT PUSH DEVELOPMENT VERSION TO GITHUB - it will break the production build
-axios.defaults.baseURL = "https://gadgetgrammarservice.onrender.com/";
+axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.withCredentials = true;
 
 // VueX Setup
